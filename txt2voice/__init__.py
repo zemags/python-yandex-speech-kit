6251cc473cc71042159def1d60a9496cff55d670
zemags <azmt2008@gmail.com>
2023-08-05 11:07:22 +0300

    Fix models import

--- txt2voice/__init__.py
+++ txt2voice/__init__.py
@@ -1,4 +1,5 @@
 """Convert text to voice"""
 from txt2voice.api import TextToVoice  # noqa
+from txt2voice.models import RequestParams, VoiceParams, SpeedParams, AudioContent  # noqa
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
